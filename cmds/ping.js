0efebc16537b6b9c71eec9db692b997328cbdd16
tr4sshy <pztrxyz@gmail.com>
2019-05-31 00:17:13 +0700

    add exports

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,3 +1,15 @@
 exports.run = (bot, msg, args) => {
     msg.channel.send(bot.ping)
 }
+
+exports.help = {
+    aliases: ["pong"],
+    name: "ping",
+    description: "show ping of the client"
+}
+
+exports.config = {
+    args: false,
+    restricted: false,
+    category: "Utility"
+}
