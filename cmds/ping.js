e399da63c000f3edd7e96dd360ea56b84799a0a2
tr4sshy <pztrxyz@gmail.com>
2019-05-31 01:12:27 +0700

    new Richembed

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,5 +1,19 @@
+const { RichEmbed } = require('discord.js');
+
 exports.run = (bot, msg, args) => {
-    msg.channel.send(bot.ping)
+    var latency = Date.now() - msg.createdTimestamp
+    var api = bot.ping.toFixed(2);
+
+    var embed = new RichEmbed()
+    .setAuthor(':ping_pong: Ping')
+    .setColor('RANDOM')
+    .addField(':speech_balloon: Latency', '`'+`${latency}ms` + '`')
+    .addField(':signal_strength: API', '`'+`${api}ms`+'`')
+    .setTimestamp()
+    .setFooter(`Requested By ${msg.author.username}`, msg.author.displayAvatarURL)
+    msg.channel.send(embed)
+
+
 }
 
 exports.help = {
