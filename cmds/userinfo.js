1635231cf0c248a4fa832b8a476cbdadf816e2d4
tr4sshy <pztrxyz@gmail.com>
2019-06-01 01:49:01 +0700

    a

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -11,9 +11,9 @@ exports.run = (bot, msg, args) => {
         var uembed = new Discord.RichEmbed()
         .setAuthor(`Info Of ${msg.author.tag}`, msg.author.displayAvatarURL)
         .setThumbnail(msg.author.displayAvatarURL)
-        .addField(`General Information`, "```"+ `Tag: ${msg.author.tag} \nid: ${msg.author.id}, \ntype: ${type[msg.author.bot]} \ncreatedAt: ${moment(msg.author.createdTimestamp).format("YYYY - M - D [at] HH-mm-s")}`+ "```")
-        .addField(`Status`, `Status: ${msg.author.presence.status} \nGame: ${msg.author.presence.game}`+ "```")
-        .addField(`Member Info`, `Joined At: ${moment(msg.member.joinedTimestamp).format("YYYY - M - D [at] HH-mm-s")} \nRoles: ${msg.member.roles.size}` + "```")
+        .addField(`General Information`, "```"+ `Tag: ${msg.author.tag} \nid: ${msg.author.id}, \ntype: ${type[msg.author.bot]} \ncreatedAt: ${moment(msg.author.createdTimestamp).format("YYYY - M - D [at] HH:mm:s")} \navatarURL: [CLICK ME](${msg.author.avatarURL}) \nMention: <@${msg.author.id}>`+ "```")
+        .addField(`Status`, "```"+`Status: ${msg.author.presence.status} \nGame: ${msg.author.presence.game}`+ "```")
+        .addField(`Member Info`, "```"+`Joined At: ${moment(msg.member.joinedTimestamp).format("YYYY - M - D [at] HH:mm:s")} \nRoles: ${msg.member.roles.size}` + "```")
         msg.channel.send(uembed)
 
     }
