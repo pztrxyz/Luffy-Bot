795723bba199a7f27a3ad22fa22020a729764d01
tr4sshy <pztrxyz@gmail.com>
2019-06-01 01:37:40 +0700

    add userinfo command

---
+++ cmds/userinfo.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+
+exports.run = (bot, msg, args) => {
+    var mention = msg.mentions.mentions.members.first() || msg.guild.members.get(args[0])
+    const type = {
+        true: "Bot", false: "Human"
+    }
+
+    if(!mention) {
+        var uembed = new Discord.RichEmbed()
+        .setAuthor(`Info Of ${msg.author.tag}`, msg.author.displayAvatarURL)
+        .setThumbnail(msg.author.displayAvatarURL)
+        .addField(`General Information`, "```"+ `Tag: ${msg.author.tag} \nid: ${msg.author.id}, \ntype: ${type[msg.author.bot]} \ncreatedAt: ${moment(msg.author.createdTimestamp).format("YYYY - M - D [at] HH-mm-s")}`+ "```")
+        .addField(`Status`, `Status: ${msg.author.presence.status} \nGame: ${msg.author.presence.game}`+ "```")
+        .addField(`Member Info`, `Joined At: ${moment(msg.member.joinedTimestamp).format("YYYY - M - D [at] HH-mm-s")} \nRoles: ${roles.size}` + "```")
+
+    }
+
+}
+
+exports.help = {
+    aliases: ["ui", "user"],
+    name: "userinfo"
+}
+
+exports.conf = {
+    restricted: false,
+    category: "Utility"
+}
