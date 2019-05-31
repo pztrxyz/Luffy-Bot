cba3493bfacd3da0825e98fe220b015d82d7da9b
tr4sshy <pztrxyz@gmail.com>
2019-06-01 00:50:58 +0700

    add status command

--- cmds/status.js
+++ cmds/status.js
@@ -1,9 +1,19 @@
-const { RichEmbed } = require('discord.js');
-require('moment')
+const { version, RichEmbed } = require('discord.js');
+const moment = require('moment');
+const os = require('os');
+const cpu = require('cpu-stat');
+
+cpu.usagePercent(function(err, percent, seconds) {
+    if (err) {
+      return console.log(err);
+    }
+
 exports.run = (bot, msg, args) => {
     var embed = new RichEmbed()
     .addField("Connected To", "```"+ `Users: ${bot.users.size} \n Servers: ${bot.guilds.size}`+"```")
-    .addField("Usage", "```"+`Uptime: ${(bot.uptime).for}`)
+    .addField("Usage", "```"+`Uptime: ${moment(bot.uptime).format(" D [days] H [hours] S [Seconds]")} \n Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB \n Cpu: ${percent.toFixed(2)}%`+ "```")
+    .addField(`System`, "```"+`CPU: ${os.cpus().map(i => `${i.model}`)[0]} \n Arch: ${os.arch()} \n PLatform: ${os.platform()} \n Nodejs: v${process.version} \n Discord.js: v${version}`) 
+    msg.channel.send(embed);
 }
 
 exports.help = {
