7975e58403ddc311cb44ca8ba996a1817c936b11
tr4sshy <pztrxyz@gmail.com>
2019-06-01 01:00:53 +0700

    a

--- cmds/status.js
+++ cmds/status.js
@@ -13,7 +13,7 @@ exports.run = (bot, msg, args) => {
     var embed = new RichEmbed()
     .addField("Connected To", "```"+ `Users: ${bot.users.size} \n Servers: ${bot.guilds.size}`+"```")
     .addField("Usage", "```"+`Uptime: ${moment(bot.uptime).format(" D [days] H [hours] S [Seconds]")} \n Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB \n Cpu: ${percent.toFixed(2)}%`+ "```")
-    .addField(`System`, "```"+`CPU: ${os.cpus().map(i => `${i.model}`)[0]} \n Arch: ${os.arch()} \n PLatform: ${os.platform()} \n Nodejs: v${process.version} \n Discord.js: v${version}`) 
+    .addField(`System`, "```"+`CPU: ${os.cpus().map(i => `${i.model}`)[0]} \n Arch: ${os.arch()} \n PLatform: ${os.platform()} \n Nodejs: ${process.version} \n Discord.js: v${version}`+"```") 
     msg.channel.send(embed);
     })
 }
