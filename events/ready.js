8c7fba7e65645cfb7f76e380f636bcf95b77963d
tr4sshy <pztrxyz@gmail.com>
2019-05-31 13:32:34 +0700

    setPresence

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
 module.exports = (bot) => {
-    console.log(`Login As [${bot.user.tag}]`)
+    console.log(`Login As [${bot.user.tag}]`);
+    bot.user.setPresence({game: {name: `With ${bot.users.size}`, type: "0", url:" "},status: "DND"});
 }
