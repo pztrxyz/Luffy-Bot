49e720fd04e3722fb614a4a4f32eda9ab7de4665
tr4sshy <pztrxyz@gmail.com>
2019-05-31 23:17:00 +0700

    add 8ball command

---
+++ cmds/8ball.js
@@ -0,0 +1,18 @@
+exports.run = (bot, msg, args) => {
+    const result = [ "NO!", "YES", "PROBABLY NOT", "MAYBE"];
+
+    const random = Math.floor(Math.random() * result.length);
+
+    msg.channel.send(result[random]);
+}
+
+exports.help = {
+    aliases: [],
+    name: "8ball"
+}
+
+exports.conf = {
+    args: true,
+    restricted: false,
+    category: "Fun"
+}
