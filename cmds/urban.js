14b93682b63fbe036cbc173093bb4c0b25e0ca73
tr4sshy <pztrxyz@gmail.com>
2019-06-01 15:38:21 +0700

    add urban

---
+++ cmds/urban.js
@@ -0,0 +1,33 @@
+const urban = require('relevant-urban');
+const Discord = require('discord.js');
+
+exports.run = async(bot, msg, args) => {
+    let res = await urban(args.join(" ")).catch(e => {
+        return msg.channel.send(":x: | Word Not Found")
+    })
+
+    var embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle(res.word)
+	    .setURL(res.urbanURL)
+	    .setDescription(`**Definition:**\n*${res.definition}*\n\n**Contoh:**\n*${res.example}*`)
+	    .addField('Writer', res.author,true)
+	    .addField('Rating', `**\`👍🏻 Upvotes: ${res.thumbsUp} | 👎🏻 Downvotes: ${res.thumbsDown}\`**`)
+	    
+	   if (res.tags.length > 0 && res.tags.join(', ').length < 1024) {
+   		embed.addField('Tags', res.tags.join(', '), true) 
+   		
+   	};
+  message.channel.send(embed)
+}
+
+exports.help = {
+    aliases: [],
+    name: "urban"
+}
+
+exports.conf = {
+    args: true,
+    restricted: false,
+    category: "Utility"
+}
